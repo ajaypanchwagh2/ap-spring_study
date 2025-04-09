dcf6221b2e867a9f649b4b2b424408acacc2a4f3
Your Name <you@example.com>
2025-04-09 17:58:13 +0530

    Deleted as it is not working

--- RestAPI-create-without-database-with-SpringBoot-master/RestAPI-create-without-database-with-SpringBoot-master/RestApiWithOutDb/src/main/java/com/RestApiWithOutDb/RestApiWithOutDb/service/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.RestApiWithOutDb.RestApiWithOutDb.service;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
